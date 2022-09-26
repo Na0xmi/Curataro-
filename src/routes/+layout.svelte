32f0d53b276b1def039b247ce99fba4154cbcd91
kal <luciankallee@gmail.com>
2022-09-26 18:10:01 +0200

    first update

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,24 @@
+<script>
+  import Header from "$lib/header/Header.svelte";
+  import "../app.postcss";
+</script>
+
+<Header />
+
+<main>
+  <slot />
+</main>
+
+<footer class="bg-color p-7">
+  
+<h1 class="text-center text-lg">Built with 🐹 by Team Hashira during MetaBuild</h1>
+
+</footer>
+
+<style>
+
+.bg-color {
+	background-color: #FFAAA6;
+  }
+ 
+</style>
